b675c8f7501ce54331d484d4f1fc723e1801b2a8
LORDBABUINO <jorge.x7@gmail.com>
2020-02-24 00:53:44 -0300

    Configurates routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
 
 import Routes from './routes'
+import history from './services/history'
 import GlobalStyle from './styles/global'
 
 const App = () => (
-  <>
+  <Router history={history}>
     <Routes />
     <GlobalStyle />
-  </>
+  </Router>
 )
 
 export default App
